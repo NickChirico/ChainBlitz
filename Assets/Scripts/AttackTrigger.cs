07bb48897a54cbc3aa38949b533c33778f44aad5
Unknown <n1spk1c@gmail.com>
2018-02-05 19:37:10 -0500

    Temp Attack
    1st attempt at a basic melee attack, and temp enemy

---
+++ Assets/Scripts/AttackTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour {
+
+	public int damage = 20;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.isTrigger == true && collider.CompareTag ("Enemy"))
+		{
+			// WHAT HAPPENS WHEN YOU ATTACK AN ENEMY
+
+			// TO-DO
+			Debug.Log("Enemy has been hit!");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+	private bool attacking = false;
+
+	public float attackTimer = 0f;
+	public float attackCD = 0.3f;
+
+	public Collider2D attackTrigger;
+
+	private Animator anim;
+
+
+	void Start ()
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		attackTrigger.enabled = false;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.JoystickButton2) && !attacking)
+		{
+			attacking = true;
+			attackTimer = attackCD;
+
+			attackTrigger.enabled = true;
+		}
+
+		if (attacking)
+		{
+			if (attackTimer > 0)
+				attackTimer -= Time.deltaTime;
+			else
+			{
+				attacking = false;
+				attackTrigger.enabled = false;
+			}
+		}
+
+		anim.SetBool ("Attacking", attacking);
+	}
+}
