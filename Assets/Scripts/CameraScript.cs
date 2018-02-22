e31181ce58f9d49522db75804cfb1e5481542b65
Unknown <n1spk1c@gmail.com>
2018-02-21 22:18:59 -0500

    Before AI

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-//	public Collider2D rightEdge;
-//	public Collider2D leftEdge;
-//	public Collider2D topEdge;
-//	public Collider2D bottomEdge;
+	//	public Collider2D rightEdge;
+	//	public Collider2D leftEdge;
+	//	public Collider2D topEdge;
+	//	public Collider2D bottomEdge;
 
 	private Vector2 velocity;
 
@@ -16,17 +16,17 @@ public class CameraScript : MonoBehaviour
 
 	public GameObject player;
 
-
-	// Use this for initialization
 	void Start ()
 	{
 		//player = GameObject.FindGameObjectWithTag ("Player");
 	}
-	
-	void fixedUpdate()
+
+	void Update ()
 	{
 		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		//float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y+2.5f, ref velocity.y, smoothTimeY); AT BOTTOM, NOT CENTER
 		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
 
 		this.transform.position = new Vector3 (posX, posY, transform.position.z);
 	}
